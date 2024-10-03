ef6280668d8c35038b667dd68b1033566d6e2376
Remmy4873 <Remmykiplangat4873@gmail.com>
2024-10-03 12:57:06 +0300

    initial commit

--- server/app.py
+++ server/app.py
@@ -1,7 +1,6 @@
-# server/app.py
 #!/usr/bin/env python3
 
-from flask import Flask, make_response
+from flask import Flask, make_response, jsonify
 from flask_migrate import Migrate
 
 from models import db, Earthquake
@@ -20,7 +19,42 @@ def index():
     body = {'message': 'Flask SQLAlchemy Lab 1'}
     return make_response(body, 200)
 
-# Add views here
+# Updated route with correct spelling
+@app.route('/earthquakes/<int:id>', methods=['GET'])
+def get_earthquake(id):
+    earthquake = Earthquake.query.get(id)
+    if earthquake:
+        response_body = {
+            "id": earthquake.id,
+            "location": earthquake.location,
+            "magnitude": earthquake.magnitude,
+            "year": earthquake.year
+        }
+        return make_response(jsonify(response_body), 200)
+    else:
+        # Fixed typo in jsonify
+        return make_response(jsonify({"message": f"Earthquake {id} not found."}), 404)
+
+
+@app.route('/earthquakes/magnitude/<float:magnitude>', methods=['GET'])
+def get_earthquakes_by_magnitude(magnitude):
+    eartquakes = Earthquake.query.filter(Earthquake.magnitude >= magnitude).all()
+
+    quakes_list = [
+        {
+            "id": quake.id,
+            "location": quake.location,
+            "magnitude": quake.magnitude,
+            "year": quake.year
+        }
+        for quake in eartquakes
+    ]
+    response_body = {
+        "count": len(quakes_list),
+        "quakes": quakes_list
+    }
+
+    return make_response(jsonify(response_body), 200)
 
 
 if __name__ == '__main__':
